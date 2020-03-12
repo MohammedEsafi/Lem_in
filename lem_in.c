37d82f5db574b5da88edd93ad8ae527eea49691d
Tareq Bareich <tbareich@student.1337.ma>
2020-03-12 21:10:23 +0100

    add usage bonus and program infos

--- lem_in.c
+++ lem_in.c
@@ -6,7 +6,7 @@
 /*   By: tbareich <tbareich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 13:39:20 by mesafi            #+#    #+#             */
-/*   Updated: 2020/03/12 19:33:56 by tbareich         ###   ########.fr       */
+/*   Updated: 2020/03/12 21:01:41 by tbareich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,12 +69,18 @@ static void		print_circuit(t_lem_in *farm)
 	t_list		*node;
 	t_list		*path;
 
-	if (farm->required_iter != -1)
+	if (farm->required_iter == -1)
+		ft_printf("\n{red}required iterations : NOT DETECTED\n");
+	else
 		ft_printf("\n{red}required iterations : %d\n", farm->required_iter);
-	ft_printf("\n{blue}total ants : %d\n", farm->ants);
-	ft_printf("total rooms : %d{eoc}\n", farm->graph->v);
-	ft_printf("\n{yellow}circuit size : %d\n", farm->circuit->size);
-	ft_printf("total edges : %d\n", farm->circuit->total_edges);
+	ft_printf("\n{blue}total ants  : %d\n", farm->ants);
+	ft_printf("total rooms : %d\n", farm->graph->v);
+	ft_printf("{start room : %s\n",
+			((t_room *)((farm->graph->adj_list[farm->start]).content))->name);
+	ft_printf("{end room   : %s\n{eoc}",
+			((t_room *)((farm->graph->adj_list[farm->end]).content))->name);
+	ft_printf("\n{yellow}circuit size     : %d\n", farm->circuit->size);
+	ft_printf("total edges      : %d\n", farm->circuit->total_edges);
 	ft_printf("total iterations : %d \n\n{eoc}", farm->circuit->score - 1);
 	if (!(farm->options & 2))
 	{
@@ -82,44 +88,55 @@ static void		print_circuit(t_lem_in *farm)
 		while (node)
 		{
 			path = ((t_path *)(node->content))->list;
-			ft_printf("{blue}[ %d ] {eoc}", ((t_path *)
+			ft_printf("{magenta}( %d ) {eoc}", ((t_path *)
 						(node->content))->ants);
-			ft_printf("{yellow}[ %d ] {eoc}:  ", ((t_path *)
+			ft_printf("{magenta}[ %d ] :{eoc}  ", ((t_path *)
 						(node->content))->size);
 			print_path(farm, path);
 			ft_printf("\n\n");
 			node = node->next;
 		}
 	}
+	ft_printf("{blue}─── infos about farm\n");
+	ft_printf("{yellow}─── infos about circuits\n");
+	ft_printf("{red}─── infos in files{eoc}\n");
 	ft_printf("\n");
+}
+
+void			usage(t_lem_in *farm, int ac, char **av)
+{
+	int			i;
+
+	i = 1;
+	while (i < ac)
+	{
+		if (ft_strequ(av[i], "--help"))
+		{
+			ft_printf("Usage : ");
+			ft_printf("./lem-in [--help | --info |--info-lite] \
+[< map_file]\n");
+			ft_printf("\toptions:\n");
+			ft_printf("\t\t--help : read usage\n");
+			ft_printf("\t\t--info: display program informations\n");
+			ft_printf("\t\t--info-lite: display program informations \
+(lite version)\n");
+			exit(0);
+		}
+		if (ft_strequ(av[i], "--info"))
+			farm->options |= 1;
+		if (ft_strequ(av[i], "--info-lite"))
+			farm->options |= 2;
+		++i;
+	}
 }
 
 int				main(int ac, char **av)
 {
 	t_lem_in	*farm;
-	int			i;
 
 	farm = ft_init();
 	if (ac > 1)
-	{
-		i = 1;
-		while (i < ac)
-		{
-			if (ft_strequ(av[i], "--help"))
-			{
-				ft_printf("./lem-in [ < map_file ]\n");
-				ft_printf("\toptions:\n");
-				ft_printf("\t\t--help : read usage\n");
-				ft_printf("\t\t--info: display circuit informations\n");
-				return (0);
-			}
-			if (ft_strequ(av[i], "--info"))
-				farm->options |= 1;
-			if (ft_strequ(av[i], "--info-lite"))
-				farm->options |= 2;
-			++i;
-		}
-	}
+		usage(farm, ac, av);
 	if (ft_reader(farm) == 1)
 		ft_error_handler(farm);
 	if (ft_finder(farm) == 1)
